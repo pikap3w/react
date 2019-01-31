b6dd311559657e72a4bf3b872eb4851182aec593
Kristen <29871623+pikap3w@users.noreply.github.com>
2019-01-31 16:26:23 -0600

    2nd assignment - remove title component

--- app-name/src/components/Title.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-const Title = (props) => {
-  const fWeight = (props.isImportant) ? "bold" : "normal";
-  return <h1 style={{color:props.color, fontWeight:fWeight}}>{props.text}</h1>;
-};
-
-export default Title;
