d4c37a16fbe04c1bfa94458fd2bdd720debd1108
Kristen <29871623+pikap3w@users.noreply.github.com>
2019-02-12 14:15:41 -0600

    voter app v2

--- voting-app-v1/src/components/Button.js
+++ voting-app-v1/src/components/Button.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import './Button.css';
+import React from 'react'
+import './Button.css'
 
-const Button = (props) => {
-  return (
-  <div className="language">
-    <button className="button" onClick={() => alert('You voted for ' + props.text)}> {props.text} </button>
-  </div>
-  );
+// const Button = props => {
+//   return (
+//     <div>
+//       <h3>{props.text}</h3>
+//       <button className='bg-gradient' onClick={() => alert('You voted for ' + props.text)}>
+//         <span>
+//           <i className='fa fa-plus' />
+//         </span>
+//       </button>
+//     </div>
+//   )
+// }
+
+class Button extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
+  updateCount = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  render () {
+    const { text } = this.props
+    return (
+      <div>
+        <button className='bg-gradient'>
+          <span>{this.state.count}</span>
+        </button>
+        <h3>{text}</h3>
+        <button className='bg-gradient' onClick={this.updateCount}>
+          <span>
+            <i className='fa fa-plus' />
+          </span>
+        </button>
+      </div>
+    )
+  }
 }
 
-export default Button;
+export default Button
